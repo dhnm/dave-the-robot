0af0cc0887aa15f1c1bfe838a65f31b4ef72ab9c
fireworker <fsttr@icloud.com>
2020-09-30 12:52:02 +0000

    role assign and nickname change functionality, TODO: test with non-admin, hearbeats, new member detection

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Dave the Robot
 
 A Discord bot called Dave!
+
+## Instructions
+
+**1. Install requirements**
+
+`pip3 install -r requirements.txt`
+
+or
+
+`pip install -r requirements.txt`
+
+**2. Create `.env` file**
+
+```
+BOT_TOKEN=your_bot_token
+SERVER_ID=your_server_id
+CHANNEL_ID=bots_channel_id
+```
+
+**3. Run**
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # Dave the Robot
 
-try:
-    import discord
-except ImportError:
-    print("Necessary libraries unavailable.")
+# Imports
+import os
+from dotenv import load_dotenv
+load_dotenv(override=True)
 
-client = discord.Client()
+from gateway import Gateway  # noqa # pylint: disable=unused-import
 
+base_url = "https://discord.com/api/v6"
+bot_token = os.getenv("BOT_TOKEN")
+server_id = os.getenv("SERVER_ID")
+channel_id = os.getenv("CHANNEL_ID")
+
+
+if __name__ == "__main__":
+    Gateway(
+        base_url=base_url,
+        bot_token=bot_token,
+        server_id=server_id,
+        channel_id=channel_id
+    ).start()
